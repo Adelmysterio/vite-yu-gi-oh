c8c7882ddc13066c8e549b66589377cca7a62b48
AdelMysterio <storliniadelmo@gmail.com>
2024-06-12 13:13:28 +0200

    implemented AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,30 @@
 <script>
 export default {
-data() {
-return {
+    data() {
+        return {
 
-}
-}
+        }
+    }
 }
 </script>
 
 <template>
-<h1>Header</h1>
+    <header>
+        <img src="https://www.pngfind.com/pngs/m/149-1499900_yugioh-logo-png-yugioh-the-dark-side-of.png" alt="logo">
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss">
+@use '../styles/partials/variables.scss' as *;
 
+header {
+    display: flex;
+    padding: 1rem;
+}
+
+img {
+    width: 5rem;
+    margin-right: 1rem;
+}
 </style>
